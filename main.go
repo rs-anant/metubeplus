02f14e6706849c8a40f8ff40049ff035d451e778
Anant Mishra <rs.anantmishra@gmail.com>
2024-06-01 16:24:13 +0530

    first commit / new repo for golang

---
+++ main.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+import "github.com/rsanantmishra/metubeplus/testutils"
+
+func main() {
+	fmt.Println("hi")
+}
---
+++ testutils/testutils.go
@@ -0,0 +1,53 @@
+package testutils
+
+import (
+	"fmt"
+	"os/exec"
+	"strings"
+	"syscall"
+)
+
+func testutils() {
+
+	url := `https://www.youtube.com/watch?v=Wz0H8HFkI9U`
+	progress := `--progress`
+	progressDelta := `--progress-delta 5` //seconds
+	filepath := `--print after_move:filepath`
+	channel := `--print after_move:channel`
+	likes := `--print after_move:like_count`
+
+	command := `./yt-dlp_x86.exe`
+	space := " "
+	var args []string
+	args = append(args, url)
+	args = append(args, progressDelta)
+	args = append(args, filepath)
+	args = append(args, channel)
+	args = append(args, likes)
+	args = append(args, progress)
+
+	arguments := strings.Join(args, space)
+	cmd := exec.Command(command)
+	cmd.SysProcAttr = &syscall.SysProcAttr{}
+	cmd.SysProcAttr.CmdLine = command + space + arguments
+
+	stdout, err := cmd.StdoutPipe()
+	cmd.Stderr = cmd.Stdout
+	if err != nil {
+		fmt.Print(err)
+	}
+
+	fmt.Println(cmd.String())
+
+	if err = cmd.Start(); err != nil {
+		fmt.Print(err)
+	}
+	for {
+		tmp := make([]byte, 1024)
+		n, err := stdout.Read(tmp)
+		fmt.Println("\r", string(tmp[:n]))
+		if err != nil {
+			break
+		}
+	}
+}
